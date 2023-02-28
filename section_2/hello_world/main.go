efba8161107bc1e3770c7d01d9317cfacf3941c8
DevJ1003 <devjoshi1384@gmail.com>
2023-02-28 22:50:13 +0530

    not

---
+++ section_2/hello_world/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+	concepts()
+
+}
+
+func concepts() {
+
+	firstName := "John"
+	firstName = "Nick"
+	firstName = `Jim`
+	// K := 15
+
+	fmt.Println(firstName)
+
+}
